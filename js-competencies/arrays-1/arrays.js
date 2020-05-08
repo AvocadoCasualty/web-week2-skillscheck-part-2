ebf2ff00b8646e428ecd2d38d4cd922f7256f657
AvocadoCasualty <baibeebambi@gmail.com>
2020-05-08 15:53:08 -0700

    As far as I got by end of Friday

--- js-competencies/arrays-1/arrays.js
+++ js-competencies/arrays-1/arrays.js
@@ -7,7 +7,7 @@
 //"red", "blue", "brown" in the array.
 
 //Code here
-
+let rubberDucks =['yellow', 'red', 'blue', 'brown']
 
 //////////////////PROBLEM 2////////////////////
 
@@ -16,13 +16,13 @@
 //What is the first numbered index of an array?
 
 // var answer = 1
-// var answer = 0
+var answer = 0
 
 //How do you access the length of an array, such as the array
 //you created above called rubberDucks?
 
 // var arrayLength = Array.length(rubberDucks)
-// var arrayLength = rubberDucks.length
+var arrayLength = rubberDucks.length
 // var arrayLength = rubberDucks.len
 // var arrayLength = rubberDucks.length()
 
@@ -35,7 +35,9 @@
 //The passed-in array should NOT be modified.
 
 //Code here
-
+function getLast(arr) {
+    return arr[arr.length-1]
+}
 
 //////////////////PROBLEM 4////////////////////
 
@@ -45,7 +47,11 @@
 //The passed-in array should NOT be modified.
 
 //Code here
-
+function getElement(arr, index) {
+    for(i = 0; i < arr.length; i++){
+       return arr[index]
+    }
+}
 
 //////////////////PROBLEM 5////////////////////
 
@@ -55,7 +61,9 @@
 //to the given value. You do not need to return the array.
 
 //Code here
-
+function changeElement(arr, index, value) {
+    arr[index] = value
+}
 
 //////////////////PROBLEM 6////////////////////
 
@@ -63,3 +71,7 @@
 //the last element. trim should return the array.
 
 //Code here
+function trim(arr) {
+    arr.splice(arr.length -1, 1)
+    return arr
+}
